351d3dcce29e6678108d83056a78a7a88c4d7290
myemcu <1142497680@qq.com>
2016-08-11 15:50:50 +0800

    V3，写入数据到手机自带的SD卡。

--- app/src/main/java/com/myemcu/app_15filestorm/MainActivity.java
+++ app/src/main/java/com/myemcu/app_15filestorm/MainActivity.java
@@ -1,15 +1,18 @@
 package com.myemcu.app_15filestorm;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.io.RandomAccessFile;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,11 +46,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void write(String s) {
         try {
-                FileOutputStream fos = openFileOutput(fileName, Context.MODE_APPEND);
-                PrintStream ps = new PrintStream(fos);
-                ps.print(s);
-                ps.close();
-                fos.close();
+               //判断手机中的SD卡是否存在
+               if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+
+                   File sdCardDir = Environment.getExternalStorageDirectory(); // 获取SD卡路径
+                   File destFile = new File(sdCardDir.getCanonicalPath()+File.separator+fileName); // 根据路径和文件名创建文件
+
+                   RandomAccessFile raf = new RandomAccessFile(destFile,"rw");
+                   raf.seek(destFile.length()); // 把指针定位到文件末尾
+                   raf.write(s.getBytes());     // 在文件末尾追加新内容
+                   raf.close();                 // 关闭文件
+               }
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -56,18 +65,25 @@ public class MainActivity extends AppCompatActivity {
 
     private String read() {
         StringBuilder sBuilder = new StringBuilder("");
-        try {
-                FileInputStream is = openFileInput(fileName);
-                byte[] buffer = new byte[64];
-                int hasRead;
-                while ((hasRead=is.read(buffer)) != -1) {
-                    sBuilder.append(new String(buffer,0,hasRead));
+        try {   //判断手机中的SD卡是否存在
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+
+                    File sdCardDir = Environment.getExternalStorageDirectory(); // 获取SD卡路径
+                    File destFile = new File(sdCardDir.getCanonicalPath()+File.separator+fileName); // 根据路径和文件名创建文件
+
+                    FileInputStream fis = new FileInputStream(destFile);        // 创建文件输入流
+                    byte[] buffer = new byte[64];                               // 定义缓冲区大小
+                    int hasread;
+                    while ((hasread=fis.read(buffer)) != -1) { // 一直循环到文件末尾
+                        sBuilder.append(new String(buffer,0,hasread)); // 在字符串后追加内容
+                    }
+                    return sBuilder.toString();
                 }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-        return sBuilder.toString();
+        return null;
     }
 
     private void findViews() {
